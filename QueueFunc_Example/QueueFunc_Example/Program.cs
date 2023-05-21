6085fa438c0a7ef0c408692ee66e2d57bcb3c6f2
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-21 13:09:45 +0330

    Add files via upload

---
+++ QueueFunc_Example/QueueFunc_Example/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueFunc_Example
+{
+    class Program
+    {
+        static void s()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Queue Q = new Queue();
+            Q.enqueue(1);
+            Q.enqueue(3);
+            Q.enqueue(4);
+            Q.enqueue(6);
+            Q.enqueue(5);
+            Q.enqueue(7);
+            Q.enqueue(2);
+
+            Console.Write("Queue Is : ");
+            Q.print();
+            Q.delmax();
+            Console.Write("Now Queue Is : ");
+            Q.print();
+            Console.Write("\n\nNow Queue Is : ");
+            Q.delbend();
+            Q.print();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QueueFunc_Example/QueueFunc_Example/Queue.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueFunc_Example
+{
+    class Queue
+    {
+        public Node rear;
+        public Node front;
+        public Queue()
+        {
+            front = rear = null;
+        }
+        public void enqueue(int d)
+        {
+            Node nn = new Node(d);
+            if (rear == null)
+            {
+                front = nn;
+                rear = nn;
+                return;
+            }
+            rear.next = nn;
+            rear = nn;
+        }
+        public void dequeue()
+        {
+            if (front == null)
+            {
+                return;
+            }
+            Console.WriteLine($"{front.data} Is Deleted...");
+            front = front.next;
+            if (front == null)
+            {
+                rear = null;
+            }
+        }
+        public void print()
+        {
+            Node n = front;
+            if (front == null && rear == null)
+            {
+                return;
+            }
+            while (n != null)
+            {
+                Console.Write(n.data + " . ");
+                n = n.next;
+            }
+        }
+        public void delmax()
+        {
+            if (front == null)
+            {
+                return;
+            }
+            int max = int.MinValue;
+            Node n = front;
+            while (n != null)
+            {
+                if (max < n.data)
+                    max = n.data;
+                n = n.next;
+            }
+            int d = max;
+            Console.WriteLine($"\n\n{max} Is Deleted...");
+            Node nn = front;
+            Node t = null;
+
+            if (nn != null && nn.data == d)
+            {
+                front = nn.next;
+                return;
+            }
+            while (nn != null && nn.data != d)
+            {
+                t = nn;
+                nn = nn.next;
+            }
+            if (nn == null)
+            {
+                return;
+            }
+            t.next = nn.next;
+        }
+        public void delbend()
+        {
+            int d = rear.data;
+            Node nn = front;
+            Node t = null;
+
+            if (nn != null && nn.data == d)
+            {
+                front = nn.next;
+                return;
+            }
+            while (nn != null && nn.data != d)
+            {
+                t = nn;
+                nn = nn.next;
+            }
+            if (nn == null)
+            {
+                return;
+            }
+            t.next = nn.next;
+        }
+
+    }
+}
